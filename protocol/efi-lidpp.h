91f15f2180803bedfe4e89170054801fa1fd96f8
Warren Mann <warrenm@gmail.com>
2018-05-11 08:54:29 -0500

    Add VGA miniport protocol

--- protocol/efi-lidpp.h
+++ protocol/efi-lidpp.h
@@ -7,6 +7,8 @@
 #ifndef __EFI_LIDPP_H
 #define __EFI_LIDPP_H
 
+#include <efi.h>
+
 
 #define EFI_LOADED_IMAGE_DEVICE_PATH_PROTOCOL_GUID  {0xbc62157e, 0x3e33, 0x4fec, {0x99, 0x20, 0x2d, 0x3b, 0x36, 0xd7, 0x50, 0xdf}}
 
